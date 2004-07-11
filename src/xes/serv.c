395a643ede6d59a75e8eba4270029837dbf0ac88
Matthieu Herrb <matthieu.herrb@laas.fr>
2004-07-11 16:57:17 +0000

    update copyright.

--- src/xes/serv.c
+++ src/xes/serv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1991-2003 CNRS/LAAS
+ * Copyright (c) 1991-2004 CNRS/LAAS
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
