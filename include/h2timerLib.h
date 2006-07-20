04fc9b20ae76095195fb5edcfe171dff3e0be135
Matthieu Herrb <matthieu.herrb@laas.fr>
2006-07-20 15:39:09 +0000

    english comments (+ one field name in h2timer structure)

--- include/h2timerLib.h
+++ include/h2timerLib.h
@@ -1,6 +1,6 @@
 /* $LAAS$ */
 /*
- * Copyright (c) 1999, 2003-2004 CNRS/LAAS
+ * Copyright (c) 1999, 2003-2006 CNRS/LAAS
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -29,30 +29,29 @@
 extern "C" {
 #endif
 
-/* Flag d'indication d'initialisation */
+/* Flag indicating initialization */
 #define  H2TIMER_FLG_INIT                 0x44444444
 
-/* Nombre max de timers */
+/* Maximum number of timers */
 #define  NMAX_TIMERS                      20
 
-/* Temps max du retard */
+/* Maximum initial delay */
 #define  MAX_DELAY                        20
 
-/* Status des timers */
+/* Possible timer States */
 #define  STOPPED_TIMER                    0
 #define  WAIT_DELAY                       1
 #define  RUNNING_TIMER                    2
 
-/* Definition de type d'un timer h2 */
+/* H2 Timer structure */
 typedef struct {
-    int flagInit;			/* Indication d'initialisation */
-    int status;				/* Etat du timer */
-    int periode;			/* Periode du timer */
-    int delay;				/* Retard pour starter timer */
-    int count;				/* Compteur du timer */
-    SEM_ID semSync;			/* semaphore de synchronisation */
+    int flagInit;			/* initialisation flag */
+    int status;				/* state of the timer */
+    int period;				/* period */
+    int delay;				/* delay */
+    int count;				/* counter */
+    SEM_ID semSync;			/* synchronisation variable */
 } H2TIMER, *H2TIMER_ID;
- 
 
 extern STATUS h2timerInit ( void );
 extern STATUS h2timerEnd ( void );
@@ -67,10 +66,10 @@ extern STATUS h2timerFree ( H2TIMER_ID timerId );
 /* -- ERRORS CODES -- */
 #include "h2errorLib.h"
 
-/* Code du module */
+/* module major number */
 #define   M_h2timerLib   504
 
-/* Codes des erreurs */
+/* Error codes */
 #define  S_h2timerLib_TIMER_NOT_INIT      H2_ENCODE_ERR(M_h2timerLib, 0)
 #define  S_h2timerLib_BAD_DELAY           H2_ENCODE_ERR(M_h2timerLib, 1)
 #define  S_h2timerLib_TOO_MUCH_TIMERS     H2_ENCODE_ERR(M_h2timerLib, 2)
