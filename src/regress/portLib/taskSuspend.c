61afa67438cb3d839846e2c8cdd0e77d79c5c10b
Matthieu Herrb <matthieu.herrb@laas.fr>
2014-05-16 18:53:49 +0200

    Hack the taskSuspend() regress test to make it success.
    Previously it was expected to fail (since the program dumps core
    when working correctly), but 'make distcheck' doesn't like that.

--- src/regress/portLib/taskSuspend.c
+++ src/regress/portLib/taskSuspend.c
@@ -16,16 +16,52 @@
 
 #include "pocolibs-config.h"
 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <err.h>
+#include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <portLib.h>
 #include <taskLib.h>
 
-int
-pocoregress_init()
+
+static int
+suspend_test(void)
 {
    logMsg("trying to suspend ourselves\n");
    taskSuspend(0);
    logMsg("not suspended...\n");
    return 0;
+}
+
+int
+pocoregress_init(void)
+{
+	pid_t pid, result;
+	int status;
+
+	pid = fork();
+	switch (pid) {
+	case -1:
+		warn("fork");
+		return 2;
+	case 0:
+		return suspend_test();
+	default:
+		do {
+			result = waitpid(pid, &status, 0);
+		} while (result == -1 && errno == EINTR);
+		if (result == pid && 
+		    WIFSIGNALED(status) && WTERMSIG(status) == SIGABRT) 
+			return 0;
+		if (result == -1) {
+			warn("waitpid");
+			return 2;
+		}
+		warnx("process didn't suspend\n");
+		return 2;
+	}
 }
